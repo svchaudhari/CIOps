3863f8949da58c2f05cf8832afed890530589ea0
svchaudhari <svchaudhari.ext@deloitte.com>
2024-08-07 10:42:48 +0530

    Update jobBuilder.groovy upyoguddhp with docker repo

--- vars/jobBuilder.groovy
+++ vars/jobBuilder.groovy
@@ -29,7 +29,7 @@ spec:
             name: jenkins-credentials
             key: dockerPassword
       - name: DOCKER_NAMESPACE
-        value: hpupyog
+        value: upyoguddhp
       - name: DOCKER_GROUP_NAME  
         value: dev
     resources:
